4953aae143b20f94a462e586d95bf8a9932af38b
ElcioLS <elciols@gmail.com>
2023-08-20 09:16:59 -0300

    Custom theme login

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -1,5 +1,7 @@
 import 'package:asyncstate/widget/async_state_builder.dart';
+import 'package:dw11_barbershop/src/core/ui/barbershop_theme.dart';
 import 'package:dw11_barbershop/src/core/ui/widgets/barbershop_loader.dart';
+import 'package:dw11_barbershop/src/features/splash/auth/login/login_page.dart';
 import 'package:dw11_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -12,10 +14,13 @@ class BarbershopApp extends StatelessWidget {
       customLoader: const BarbershopLoader(),
       builder: (AsyncNavigatorObserver) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'Dart Week 2023',
+          theme: BarbershopTheme.themeData,
           navigatorObservers: [AsyncNavigatorObserver],
           routes: {
             '/': (_) => const SplashPage(),
+            '/auth/login': (_) => const LoginPage(),
           },
         );
       },
