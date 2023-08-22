fdec02493255fedb119069f4c2940c0db72e973e
ElcioLS <elciols@gmail.com>
2023-08-21 21:23:31 -0300

    User account create

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -3,6 +3,7 @@ import 'package:dw11_barbershop/src/core/ui/barbershop_nav_global_key.dart';
 import 'package:dw11_barbershop/src/core/ui/barbershop_theme.dart';
 import 'package:dw11_barbershop/src/core/ui/widgets/barbershop_loader.dart';
 import 'package:dw11_barbershop/src/features/splash/auth/login/login_page.dart';
+import 'package:dw11_barbershop/src/features/splash/register/user_register_page.dart';
 import 'package:dw11_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -23,6 +24,7 @@ class BarbershopApp extends StatelessWidget {
           routes: {
             '/': (_) => const SplashPage(),
             '/auth/login': (_) => const LoginPage(),
+            '/auth/register/user': (_) => const UserRegisterPage(),
             '/home/adm': (_) => const Text('ADM'),
             '/home/employee': (_) => const Text('Employee'),
           },
