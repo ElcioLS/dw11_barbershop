3afaa89a2e6f9b427f792c6bcc4167527563897a
ElcioLS <elciols@gmail.com>
2023-08-30 20:09:11 -0300

    Add Employee - VM & State

---
+++ lib/src/features/employee/register/employee_register_vm.dart
@@ -0,0 +1,34 @@
+import 'package:dw11_barbershop/src/features/employee/register/employee_register_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'employee_register_vm.g.dart';
+
+@riverpod
+class EmployeeRegisterVm extends _$EmployeeRegisterVm {
+  @override
+  EmployeeRegisterState build() => EmployeeRegisterState.initial();
+
+  void setRegisterAdm(bool isRegisterAdm) {
+    state = state.copyWith(registerAdm: isRegisterAdm);
+  }
+
+  void addOrRemoveWorkdays(String weekDay) {
+    final EmployeeRegisterState(:workdays) = state;
+    if (workdays.contains(weekDay)) {
+      workdays.remove(weekDay);
+    } else {
+      workdays.add(weekDay);
+    }
+    state = state.copyWith(workdays: workdays);
+  }
+
+  void addOrRemoveWorkhours(int hour) {
+    final EmployeeRegisterState(:workhours) = state;
+    if (workhours.contains(hour)) {
+      workhours.remove(hour);
+    } else {
+      workhours.add(hour);
+    }
+    state = state.copyWith(workhours: workhours);
+  }
+}
