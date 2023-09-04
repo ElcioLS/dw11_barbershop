aff681644d8dd33d5255ed4825b3afba204751e0
ElcioLS <elciols@gmail.com>
2023-09-03 21:57:15 -0300

    Calendar Datasource implements

--- lib/src/features/employee/schedule/appointment_ds.dart
+++ lib/src/features/employee/schedule/appointment_ds.dart
@@ -1,21 +1,34 @@
+import 'package:dw11_barbershop/src/core/ui/constants.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
+import '../../../model/schedule_model.dart';
+
 class AppointmentDs extends CalendarDataSource {
+  final List<ScheduleModel> schedules;
+
+  AppointmentDs({
+    required this.schedules,
+  });
+
   @override
-  List<dynamic>? get appointments => [
-        Appointment(
-            startTime: DateTime.now(),
-            endTime: DateTime.now().add(
-              const Duration(hours: 1),
-            ),
-            subject: 'Elcio Lopes'),
-        Appointment(
-            startTime: DateTime.now().add(
-              const Duration(hours: 2),
-            ),
-            endTime: DateTime.now().add(
-              const Duration(hours: 3),
-            ),
-            subject: 'Alicio Lopes'),
-      ];
+  List<dynamic>? get appointments {
+    return schedules.map((e) {
+      final ScheduleModel(
+        date: DateTime(:year, :month, :day),
+        :hour,
+        :clientName,
+      ) = e;
+
+      final startTime = DateTime(year, month, day, hour, 0, 0);
+      final endTime = DateTime(year, month, day, hour + 1, 0, 0);
+
+      return Appointment(
+          color: ColorsConstants.brow,
+          startTime: DateTime.now(),
+          endTime: DateTime.now().add(
+            const Duration(hours: 1),
+          ),
+          subject: clientName);
+    }).toList();
+  }
 }
